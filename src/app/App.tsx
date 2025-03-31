bf833455152cbffd6b1ae08a296fe5a61dee40e5
sealjuli <ms.yulsh@mail.ru>
2025-03-31 11:38:44 +0300

    init project

---
+++ src/app/App.tsx
@@ -0,0 +1,12 @@
+import { JSX } from 'react'
+import { Form } from 'antd'
+import './App.css'
+
+export const App = (): JSX.Element => {
+  return (
+    <>
+      <p>New</p>
+      <Form></Form>
+    </>
+  )
+}
